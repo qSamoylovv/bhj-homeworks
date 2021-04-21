c1d1e8a6ea161ff86cb1b714ad6085815865710e
Samoylov Zakhar <samoilov.zahar20121@gmail.com>
2021-04-22 00:52:51 +0300

    Homework mole-game modified and completed

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -7,27 +7,26 @@ function clickBoard() {
     const hole = document.querySelectorAll('.hole');
 
     hole.forEach((elem) => {
-        function clickMole() {
-            if (elem.classList.contains('hole_has-mole')) {
-                deadCounter.textContent = +deadCounter.textContent + 1;
-            } else {
-                lostCounter.textContent = +lostCounter.textContent + 1;
-            }
-
-            console.log(lostCounter.textContent);
-            if (deadCounter.textContent == 10) {
-                alert('Победа!');
-                location.reload();
-            }
-
-            if (lostCounter.textContent == 5) {
-                alert('Вы проиграли');
-                location.reload();
-            }
-        }
-
         elem.addEventListener('click', clickMole);
     });
+
+    function clickMole() {
+        if (this.classList.contains('hole_has-mole')) {
+            deadCounter.textContent = +deadCounter.textContent + 1;
+        } else {
+            lostCounter.textContent = +lostCounter.textContent + 1;
+        }
+
+        if (deadCounter.textContent == 10) {
+            alert('Победа!');
+            document.location.reload();
+        }
+
+        if (lostCounter.textContent == 5) {
+            alert('Вы проиграли');
+            document.location.reload();
+        }
+    }
 }
 
 clickBoard();
