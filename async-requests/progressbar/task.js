5e29f4e1b693574599d894c22d9043c8c75c9519
Samoylov Zakhar <samoilov.zahar20121@gmail.com>
2021-05-17 18:01:57 +0300

    Homework progressbar completed

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function loadingFile() {
+    const progress = document.getElementById('progress');
+    const form = document.getElementById('form');
+
+    const sendForm = (e) => {
+        e.preventDefault();
+
+        let input = document.getElementsByName('file')[0];
+        let fileInput = input.files[0];
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+
+        const getRequest = (e) => (progress.value = Math.ceil((e.loaded * 100) / e.total) / 100);
+
+        xhr.upload.addEventListener('progress', getRequest);
+        xhr.send(fileInput);
+    };
+
+    form.addEventListener('submit', sendForm);
+}
+
+loadingFile();
