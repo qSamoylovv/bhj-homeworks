1aabd59a78806252c63fe5901f0f59c82eefc48b
Samoylov Zakhar <samoilov.zahar20121@gmail.com>
2021-05-18 22:04:45 +0300

    Homework progressbar modified and completed

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -12,7 +12,10 @@ function loadingFile() {
         let xhr = new XMLHttpRequest();
         xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
 
-        const getRequest = (e) => (progress.value = Math.ceil((e.loaded * 100) / e.total) / 100);
+        const getRequest = (e) => {
+            let persentLoad = Math.ceil((e.loaded * 100) / e.total) / 100;
+            progress.value = persentLoad;
+        };
 
         xhr.upload.addEventListener('progress', getRequest);
         xhr.send(fileInput);
